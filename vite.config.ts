9a51f211db8340c4f0ceb5b6346a4661eb76a649
DeepakWarude <deepakwarude009@gmail.com>
2025-09-29 18:11:12 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -51,16 +51,11 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
       open: true,
     },
-    export default {
-  build: {
-    outDir: 'dist', // <-- change this if it's 'build'
-  },
-};
 
   });
